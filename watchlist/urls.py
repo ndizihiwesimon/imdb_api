c46bc31b755e2aecb41d11bb87be2c5da47e30d9
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-18 09:09:47 +0200

    Movie details using JsonResponse

--- watchlist/urls.py
+++ watchlist/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from watchlist.views import movie_list
+from watchlist.views import movie_details, movie_list
 
 urlpatterns = [
     path('list/', movie_list, name='movie-list'),
+    path('<int:pk>/', movie_details, name='movie-details'),
 ]
 
