76d1855c1ca5ffff1a83d119d31dd5ab2875367b
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-18 09:42:25 +0200

    Added api_view decorator, created list and details

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from watchlist.views import movie_details, movie_list
+from watchlist.api.views import movie_details, movie_list
 
 urlpatterns = [
     path('list/', movie_list, name='movie-list'),
