7fc4eb3839538d261c6dfe552cb9c540b19214e3
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-20 16:15:41 +0200

    Added relationship -- platform

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from watchlist.api.views import StreamPlatformAV, StreamPlatformDetailsAV, WatchListAV, WatchDetailsAV
 
 urlpatterns = [
-    path('stream/list', StreamPlatformAV.as_view(), name='stream-list'),
+    path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
     path('stream/<int:pk>', StreamPlatformDetailsAV.as_view(), name='stream-details'),
     path('list/', WatchListAV.as_view(), name='Watch-list'),
     path('<int:pk>/', WatchDetailsAV.as_view(), name='WatchList-details'),
