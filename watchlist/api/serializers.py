75d53d80d322ea35250a58b03085e6c53ed71c35
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-20 16:36:39 +0200

    Done with nested relationships

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -5,12 +5,6 @@ from rest_framework import serializers
 from watchlist.models import StreamPlatform, WatchList
 
 
-class StreamSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StreamPlatform
-        fields = "__all__"
-
-
 class WatchListSerializer(serializers.ModelSerializer):
     len_name = serializers.SerializerMethodField()
 
@@ -21,6 +15,14 @@ class WatchListSerializer(serializers.ModelSerializer):
 
     def get_len_name(self, object):
         return len(object.title)
+
+
+class StreamSerializer(serializers.ModelSerializer):
+    watchlist = WatchListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = StreamPlatform
+        fields = "__all__"
 
 
 # def name_length(value):
