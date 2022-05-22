472e19d94362d7e6010a5a44fc895ee0c885d4fa
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-22 07:13:16 +0200

    Created review user

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -6,6 +6,8 @@ from watchlist.models import Review, StreamPlatform, WatchList
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    review_user = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Review
         # fields = "__all__"
