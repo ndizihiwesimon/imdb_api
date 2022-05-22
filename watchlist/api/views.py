33524a2e01462c1afae0e4c1da068116068f7f5d
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-22 23:43:27 +0200

    Removed unused imports

--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import generics
-from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.validators import ValidationError
 from watchlist.api.serializers import ReviewSerializer, StreamSerializer, WatchListSerializer
