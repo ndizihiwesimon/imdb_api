269e4e3ee678b0c798578efcf0771e61cb289b19
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-22 22:58:10 +0200

    Added api-auth url

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('watch/', include('watchlist.api.urls'))
+    path('watch/', include('watchlist.api.urls')),
+    path('api-auth', include('rest_framework.urls'))
 ]
