fce931920e661a98fd734c4070a639e10d1cb990
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-23 10:04:21 +0200

    Modified isAdmin or read only

--- watchlist/api/permissions.py
+++ watchlist/api/permissions.py
@@ -4,9 +4,10 @@ from rest_framework import permissions
 class IsAdminOrReadOnly(permissions.IsAdminUser):
 
     def has_permission(self, request, view):
-        # admin_permission = super().has_permission(request, view)
-        admin_permission = bool(request.user and request.user.is_staff)
-        return request.method == "GET" or admin_permission
+        if request.methods in permissions.SAFE_METHODS:
+            return True
+        else:
+            bool(request.user and request.user.is_staff)
 
 
 class ReviewUserOrReadOnly(permissions.BasePermission):
