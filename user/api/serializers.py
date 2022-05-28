95bf2caff7ab960732782228d03c73dca81d122e
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-28 09:47:48 +0200

    Dictonary issue on extra kwargs password

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -4,11 +4,11 @@ from rest_framework import serializers
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(
-        style={"input_type": "password"}, write_only=True)
+        style={'input_type': 'password'}, write_only=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'password2']
         extra_kwargs = {
-            'password': 'write_only=True'
+            'password': {'write_only': True}
         }
