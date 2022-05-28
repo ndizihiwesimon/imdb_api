ce4bd51b06e95109cb7d912db78ea97e3950a48a
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-28 09:46:32 +0200

    Fixed dictionary issue on input type

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(
-        style="input_type = password", write_only=True)
+        style={"input_type": "password"}, write_only=True)
 
     class Meta:
         model = User
