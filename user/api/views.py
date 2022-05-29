8ffcce42cc85cceed6800e2e361301df6f556551
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-05-29 23:27:02 +0200

    imported user app models in user views

--- user/api/views.py
+++ user/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from user import models
 from user.api.serializers import RegistrationSerializer
 
 
