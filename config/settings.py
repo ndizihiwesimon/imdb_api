7f8c9cd72821e852ff45431f69099406118f4ca9
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-11 17:22:58 +0200

    Changed throttle rate

--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle'
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
+        'anon': '1/day',
+        'user': '3/day'
     }
 }
