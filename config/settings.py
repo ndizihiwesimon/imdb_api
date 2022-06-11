9ebc15a5e6480f55e6202b5fab61f5734b1eea44
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-11 18:53:55 +0200

    Added rates  for create and list of reviews

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,8 @@ REST_FRAMEWORK = {
     # ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '5/day',
-        'user': '10/day'
+        'user': '10/day',
+        'review-create': '1/day',
+        'review-list': '1/day',
     }
 }
