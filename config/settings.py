046c66b5b4045e3aaa8374ead4707d7513ccf193
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-12 07:51:41 +0200

    Changed review create throttle to 5 per day

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_RATES': {
         'anon': '5/day',
         'user': '10/day',
-        'review-create': '1/day',
+        'review-create': '5/day',
         'review-list': '10/day',
     }
 }
