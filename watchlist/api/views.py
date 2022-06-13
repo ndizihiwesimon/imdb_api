943ad1574b87c9f3d6bab894019d46809e7d6190
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-13 19:24:49 +0200

    Added  query parameters

--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -19,8 +19,12 @@ class UserReview(generics.ListAPIView):
     # permission_classes = [IsAuthenticated]
     # throttle_classes = [ReviewListThrottle, AnonRateThrottle]
 
+    # def get_queryset(self):
+    #     username = self.kwargs.get('username')
+    #     return Review.objects.filter(review_user__username=username)
+
     def get_queryset(self):
-        username = self.kwargs.get('username')
+        username = self.request.query_params.get('username')
         return Review.objects.filter(review_user__username=username)
 
 
