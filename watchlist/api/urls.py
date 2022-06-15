62ad5555244239ca62c23d7d1d4dc062fdf92e92
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-15 08:03:49 +0200

    Added new watchlist route

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from watchlist.api.views import ReviewCreate, ReviewDetails, ReviewList, StreamPlatformVS, UserReview, WatchListAV, WatchDetailsAV
+from watchlist.models import WatchList
 
 router = DefaultRouter()
 router.register('stream', StreamPlatformVS, basename='stream-platform')
@@ -15,11 +16,12 @@ urlpatterns = [
 
     path('list/', WatchListAV.as_view(), name='Watch-list'),
     path('<int:pk>/', WatchDetailsAV.as_view(), name='WatchList-details'),
-
+    path('new-list/', WatchList.as_view(), name='WatchList-list2'),
     path('<int:pk>/review-create/',
          ReviewCreate.as_view(), name="review-create"),
     path('<int:pk>/reviews/', ReviewList.as_view(), name="review-list"),
     path('review/<int:pk>/', ReviewDetails.as_view(), name="review-details"),
 
-    path('user-review/', UserReview.as_view(), name="user-review-details")
+    path('user-review/', UserReview.as_view(), name="user-review-details"),
+    
 ]
