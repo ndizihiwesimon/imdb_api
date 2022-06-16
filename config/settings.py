c4e3e5b167d80e25c6000a59044adfc457e132b9
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-16 16:48:44 +0200

    Commented default pagination class

--- config/settings.py
+++ config/settings.py
@@ -153,6 +153,6 @@ REST_FRAMEWORK = {
         'review-list': '10/day',
         'review-detail': '2/day',
     },
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE': 5
 }
