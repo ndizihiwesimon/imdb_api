aa98129d69fa3ad17c0681490601ee99e1abcdd9
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-16 17:02:43 +0200

    Pagination configuration

--- watchlist/api/pagination.py
+++ watchlist/api/pagination.py
@@ -2,5 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 class WatchListPagination(PageNumberPagination):
     """Defining a pagination"""
-    page_size = 10
-    
+    page_size = 7
+    page_query_param = 'p'
+    page_size_query_param = 'size'
