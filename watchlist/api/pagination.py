ca37b3f0850665b0aeb96bdb1e6748686585c61b
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-16 17:12:44 +0200

    Commented some params

--- watchlist/api/pagination.py
+++ watchlist/api/pagination.py
@@ -3,7 +3,7 @@ from rest_framework.pagination import PageNumberPagination
 class WatchListPagination(PageNumberPagination):
     """Defining a pagination"""
     page_size = 7
-    page_query_param = 'p'
+    # page_query_param = 'p'
     page_size_query_param = 'size'
     max_page_size = 10
-    last_page_strings = 'end'
+    # last_page_strings = 'end'
