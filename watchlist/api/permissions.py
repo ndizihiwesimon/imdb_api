ca896a1325041e68b4ba25d6f24ae3db8c5d0f52
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-16 15:39:06 +0200

    Resolved method issues

--- watchlist/api/permissions.py
+++ watchlist/api/permissions.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 class IsAdminOrReadOnly(permissions.IsAdminUser):
 
     def has_permission(self, request, view):
-        if request.methods in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
         else:
             return bool(request.user and request.user.is_staff)
