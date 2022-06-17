ae7495cebe2ac7a943e919a9218b29ed2a74da6b
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-17 14:26:39 +0200

    Tested user register

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+        data = {
+            "username": "testcase",
+            "email": "testcase@example.com",
+            "password": "NewPassword@123",
+            "password2": "NewPassword@123"
+        }
+        response = self.client.post(reverse('register'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_OK) 
