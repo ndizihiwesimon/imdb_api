513e22314d345d92ddb6c2fd23636bbebf689d0b
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-17 11:15:01 +0200

    params

--- watchlist/api/pagination.py
+++ watchlist/api/pagination.py
@@ -10,3 +10,5 @@ class WatchListPagination(PageNumberPagination):
 
 class WatchListLOPagination(LimitOffsetPagination):
     default_limit = 5
+    limit_query_param = 'limit'
+    offset_query_param = 'start'
