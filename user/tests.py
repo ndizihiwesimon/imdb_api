03035806827c3c480548d57790b801e2f48ef693
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-18 23:56:06 +0200

    Imported Token model

--- user/tests.py
+++ user/tests.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.authtoken import Token
 
 class RegisterTestCase(APITestCase):
 
