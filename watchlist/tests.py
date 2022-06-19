37e8f3da35cb36837ef68528d66c0d275280af78
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-19 09:48:24 +0200

    Test stream platform create

--- watchlist/tests.py
+++ watchlist/tests.py
@@ -1,3 +1,4 @@
+import imp
 from django.contrib.auth.models import User
 from django.urls import reverse
 
@@ -5,3 +6,14 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 
+from watchlist.api import serializers
+from watchlist import models
+
+class StreamPlatformTestCase(APITestCase):
+
+    def test_stream_platform_create(self):
+        data = {
+            "name": "Netflix",
+            "about": "#1 Streaming platform",
+            "website": "http://example.com"
+        }
