8fd98c42faf088fa0ebfee09139a48cec1151e9f
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-19 09:52:01 +0200

    Finishing stream platform create test

--- watchlist/tests.py
+++ watchlist/tests.py
@@ -1,5 +1,6 @@
 from cgitb import reset
 import imp
+from os import stat
 from urllib import response
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -20,3 +21,4 @@ class StreamPlatformTestCase(APITestCase):
             "website": "https://netflix.com"
         }
         response = self.client.post(reverse('stream-platform-list'), data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
