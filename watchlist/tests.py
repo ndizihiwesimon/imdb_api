63969c614938f830041cc5b00b4de0b25c60d9c3
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-21 18:11:44 +0200

    HTTP_400_BAD_REQUEST

--- watchlist/tests.py
+++ watchlist/tests.py
@@ -113,7 +113,7 @@ class ReviewTestCase(APITestCase):
 
         response = self.client.post(
             reverse('review-create', args=(self.watchList.id, )), data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
     # def test_review_create(self):
